4e01de298f3b507fc7ac6df1cf7ffc4f3402fc1e
Jonathan Bouwer <jonothanbouwer@businessoptics.co.za>
2016-07-06 15:49:07 +0200

    Reformatted code and added definitions for a number of classes

--- ecs_view/aws_classes.py
+++ ecs_view/aws_classes.py
@@ -1,13 +1,53 @@
 class Cluster:
 
-    def __init__(self):
-        return
+    def __init__(self, name, tasks, instances):
+        self.name = name
+        self.tasks = tasks
+        self.instances = instances
+
+    def get_name(self):
+        return self.name
+
+    def set_name(self, name):
+        self.name = name
+
+    def get_tasks(self):
+        return self.tasks
+
+    def set_tasks(self, tasks):
+        self.tasks = tasks
+
+    def get_instances(self):
+        return self.instances
+
+    def set_instances(self, instances):
+        self.instances = instances
 
 
 class Task:
 
-    def __init__(self):
-        return
+    def __init__(self, definition, containers, instances):
+        self.definition = definition
+        self.containers = containers
+        self.instances = instances
+
+    def get_definition(self):
+        return self.definition
+
+    def set_definition(self, definition):
+        self.definition = definition
+
+    def get_containers(self):
+        return self.containers
+
+    def set_containers(self, containers):
+        self.containers = containers
+
+    def get_instances(self):
+        return self.instances
+
+    def set_instances(self, instances):
+        self.instances = instances
 
 
 class Instance:
@@ -24,11 +64,23 @@ class AutoScalingGroup:
 
 class Container:
 
-    def __init__(self):
-        return
+    def __init__(self, task):
+        self.task = task
+
+    def get_task(self):
+        return self.task
+
+    def set_task(self, task):
+        self.task = task
 
 
 class TaskDefinition:
 
-    def __init__(self):
-        return
+    def __init__(self, tasks):
+        self.tasks = tasks
+
+    def get_task(self):
+        return self.tasks
+
+    def set_task(self, tasks):
+        self.tasks = tasks
--- ecs_view/server.py
+++ ecs_view/server.py
@@ -21,8 +21,10 @@ boto3.setup_default_session(region_name='us-east-1')
 ecs = boto3.client('ecs')
 ec2 = boto3.resource('ec2')
 
+
 def jp(j):
     print(json.dumps(j, indent=2, default=lambda x: str(x) if isinstance(x, datetime) else json.dumps(x)))
+
 
 @functools.lru_cache(maxsize=None)
 def get_task_definition(arn):
@@ -30,8 +32,9 @@ def get_task_definition(arn):
         taskDefinition=arn
     )['taskDefinition']
 
+
 def get_cluster_overview(cluster_name):
-    instances = defaultdict(lambda: {'tasks':[]})
+    instances = defaultdict(lambda: {'tasks': []})
 
     task_arns = ecs.list_tasks(
             cluster=cluster_name,
@@ -47,7 +50,7 @@ def get_cluster_overview(cluster_name):
 
     for task in tasks:
         ci = task['containerInstanceArn']
-        containers = [{ 'arn': t['containerArn'] } for t in task['containers']]
+        # containers = [{'arn': t['containerArn'] } for t in task['containers']]
         instances[ci]['tasks'].append(task)
         instances[ci]['arn'] = ci
 
@@ -57,31 +60,31 @@ def get_cluster_overview(cluster_name):
 
     instances = list(instances.values())
 
-    cis = { ci["containerInstanceArn"]: ci for ci in  ecs.describe_container_instances(
+    cis = {ci["containerInstanceArn"]: ci for ci in ecs.describe_container_instances(
             cluster=cluster_name,
             containerInstances=[i['arn'] for i in instances]
             )['containerInstances']
-          }
+           }
 
     for instance in instances:
         ci = cis[instance['arn']]
         instance['ec2InstanceId'] = ci['ec2InstanceId']
         instance['containerInstance'] = ci
 
-    ec2InstanceIds = [ ci['ec2InstanceId'] for ci in cis.values() ]
-    ec2Instances = { ec2Instance.instance_id:ec2Instance
-                        for ec2Instance in ec2.instances.filter(InstanceIds=ec2InstanceIds)
-                   }
+    ec2InstanceIds = [ci['ec2InstanceId'] for ci in cis.values()]
+    ec2Instances = {ec2Instance.instance_id: ec2Instance
+                    for ec2Instance in ec2.instances.filter(InstanceIds=ec2InstanceIds)
+                    }
 
     for instance in instances:
         ec2Instance = ec2Instances[instance['ec2InstanceId']]
         assert instance['ec2InstanceId'] == ec2Instance.instance_id
-        tags = { tag['Key'] : tag['Value'] for tag in ec2Instance.tags }
+        tags = {tag['Key']: tag['Value'] for tag in ec2Instance.tags}
         name = tags.get('Name', '')
         instance['name'] = name
         instance['ec2Instance'] = ec2Instance
 
-    instances.sort(key = lambda i: i['name'])
+    instances.sort(key=lambda i: i['name'])
 
     families = list(families.items())
     families.sort(key=lambda x: x[0])
@@ -90,15 +93,17 @@ def get_cluster_overview(cluster_name):
 
 app = Flask(__name__, static_url_path='')
 
+
 @app.route('/cluster/<cluster_name>/', methods=['GET', 'POST'])
 def overview(cluster_name):
     instances, families = get_cluster_overview(cluster_name)
     return render_template('overview.html', instances=instances, families=families, cluster_name=cluster_name)
 
+
 @app.route('/static/<path:path>')
 def send_staticfles(path):
     return send_from_directory('static', path)
 
+
 if __name__ == '__main__':
     app.run(debug=True)
-
