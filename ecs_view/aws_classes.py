3bd703a0ce62a625069dc685b5dee11a272f77f0
Adam Kaliski <adam.kaliski47@gmail.com>
2016-07-06 14:49:02 +0200

    Created skeleton of aws classes.

---
+++ ecs_view/aws_classes.py
@@ -0,0 +1,34 @@
+class Cluster:
+
+    def __init__(self):
+        return
+
+
+class Task:
+
+    def __init__(self):
+        return
+
+
+class Instance:
+
+    def __init__(self):
+        return
+
+
+class AutoScalingGroup:
+
+    def __init__(self):
+        return
+
+
+class Container:
+
+    def __init__(self):
+        return
+
+
+class TaskDefinition:
+
+    def __init__(self):
+        return
