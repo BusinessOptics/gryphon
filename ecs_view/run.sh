2797951a7e51cb3b17efcd463c41578a615a685d
Adam Kaliski <adam.kaliski47@gmail.com>
2016-07-13 17:41:33 +0200

    Changed directory structure and edited bash scripts.

--- ecs_view/run.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -uex
-if [ "$DEV_MODE" != 1 ];
-then
-	gunicorn -c gunicorn_config.py server:app -u root
-else
-	python3 server.py
-fi
--- run-dev.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-set -uex
-docker run --net="host" -e DEV_MODE=1 -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -v `pwd`:/web-server/ -w /web-server/ecs_view -i -t -P gryphon
