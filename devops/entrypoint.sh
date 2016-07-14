eb3d6d0fdcb5a427dbc7f6a818b858aebadd7d13
Jonathan Bouwer <jonothanbouwer@businessoptics.co.za>
2016-07-14 12:00:39 +0200

    Changed entry point to change directory before executing gunicorn commands

--- devops/entrypoint.sh
+++ devops/entrypoint.sh
@@ -2,7 +2,8 @@
 set -uex
 if [ "$DEV_MODE" != 1 ];
 then
-	gunicorn -c /web-server/ecs_view/gunicorn_config.py server:app -u root
+    cd /web-server/ecs_view
+	gunicorn -c gunicorn_config.py server:app -u root
 else
 	python3 /web-server/ecs_view/server.py
 fi
