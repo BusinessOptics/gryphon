c40fe2167ec3f38ecd4caa88dd478836df308ca2
James Saunders <james@businessoptics.biz>
2016-07-15 03:32:01 +0200

    use defaults for static file handling

--- ecs_view/server.py
+++ ecs_view/server.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, send_from_directory
 
 from aws_classes import *
 
-app = Flask(__name__, static_url_path='')
+app = Flask(__name__)
 
 @app.route('/health')
 def hello():
@@ -27,12 +27,6 @@ def task_definitions():
     authorization_data = get_authorization()
     task_definitions = get_task_def_list()
     return render_template('definitions.html', task_definitions=task_definitions, should_exec=False, auth_data=authorization_data)
-
-
-@app.route('/static/<path:path>')
-def send_staticfles(path):
-    return send_from_directory('static', path)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
